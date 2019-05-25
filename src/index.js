3f23cd74e537d7374af8f36b698f6a3fb1f22ff6
AlbertoWagner <alberto.oliveira@ccc.ufcg.edu.br>
2019-05-24 22:12:39 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Calculadora from './main/Calculator'
+
+
+const elemento = document.getElementById('root')
+ReactDOM.render( < div >
+        <
+        h1 > Calculadora < /h1> <
+        Calculadora / >
+        <
+        /div>, elemento);
